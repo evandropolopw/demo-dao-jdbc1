ea68f6d1d0414757d2ca07ae3f3d24752899bf2b
evandropolopw <evandro@pointware.com.br>
2023-07-28 20:30:09 -0300

    findAll implementation

--- src/application/Program02.java
+++ src/application/Program02.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import modal.dao.DaoFactory;
 import modal.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program02 {
@@ -10,9 +13,26 @@ public class Program02 {
 		// TODO Auto-generated method stub
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: Seller findById ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println("\n=== TEST 2: Seller findByDepartmentID ===");
+		
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+        System.out.println("\n=== TEST : Seller findByAll ===");
+		
+		list = sellerDao.findAll();
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
